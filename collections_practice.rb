67a4ca7ada94b2e28c168d5d0a22f0cbdc08b2fd
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-18 06:06:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort do |el1, el2|
+    el2 <=> el1
+  end
+end
+
+def sort_array_char_count(arr)
+  arr.sort do |el1, el2|
+    el1.length <=> el2.length
+  end
+end
+
+def swap_elements(arr)
+  arr.sort do |el1, el2|
+    el1[1] <=> el2[2]
+  end
+end
+
+def reverse_array(arr)
+  arr.reverse do |el1, el2|
+    el2 <=> el1
+  end
+end
+
+def kesha_maker(arr)
+  arr.each do |string|
+    string[2] = "$"
+  end
+end
+
+def find_a(arr)
+  arr.each do |string|
+    if string[0] == "a"
+      string
+    end
+  end
+end
+
+def sum_array(arr)
+  arr.each.inject do |num|
+    num += num
+  end
+end
+
+def add_s(arr)
+  arr.each_with_index.collect do |word, i|
+    unless word[2]
+      word + "s"
+    end
+  end
+end
